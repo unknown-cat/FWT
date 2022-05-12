590d0da02aa72823af7dca26acb90c45e77cb9e9
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 19:39:15 +0300

    add react select elements, customise dropdowm indicator

--- src/components/SelectAuthor/SelectAuthor.component.jsx
+++ src/components/SelectAuthor/SelectAuthor.component.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Select, { components } from 'react-select';
+import { DropDownIcon } from '../../globalStyles';
+import { AuthorWrapper } from './selectAuthor.styles';
+
+const DropdownIndicator = (props) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <DropDownIcon />
+    </components.DropdownIndicator>
+  );
+};
 
 const SelectAuthor = () => {
   return (
-    <div>SelectAuthor</div>
-  )
-}
+    <AuthorWrapper>
+      <Select
+        classNamePrefix='custom-select'
+        isClearable
+        placeholder='Actors'
+        components={{ DropdownIndicator }}
+        styles={{
+          dropdownIndicator: (provided, state) => ({
+            ...provided,
+            transform: state.selectProps.menuIsOpen && 'rotate(180deg)',
+          }),
+        }}
+      ></Select>
+    </AuthorWrapper>
+  );
+};
 
-export default SelectAuthor
+export default SelectAuthor;
--- src/components/SelectLocation/SelectLocation.component.jsx
+++ src/components/SelectLocation/SelectLocation.component.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Select, { components } from 'react-select';
+import { DropDownIcon } from '../../globalStyles';
+import { LocationWrapper } from './selectLocation.styles';
+
+const DropdownIndicator = (props) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <DropDownIcon />
+    </components.DropdownIndicator>
+  );
+};
 
 const SelectLocation = () => {
-  return (
-    <div>SelectLocation</div>
-  )
-}
 
-export default SelectLocation
+  return (
+    <LocationWrapper>
+      <Select
+        classNamePrefix='custom-select'
+        isClearable
+        placeholder='Locations'
+        components={{ DropdownIndicator }}
+        styles={{
+          dropdownIndicator: (provided, state) => ({
+            ...provided,
+            transform: state.selectProps.menuIsOpen && 'rotate(180deg)',
+          }),
+        }}
+      ></Select>
+    </LocationWrapper>
+  );
+};
+
+export default SelectLocation;
