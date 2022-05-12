91de831bd7a18d16887f2db9c8a7f5e520b700e1
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 19:57:36 +0300

    fetch data and pass props to gallery component

--- src/context.jsx
+++ src/context.jsx
@@ -1,15 +1,26 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { getData } from './api/api';
+
+const API_PAINTINGS_ENDPONT = 'https://test-front.framework.team/paintings?q=';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
 
   const toggleTheme = () =>
     theme === 'light' ? setTheme('dark') : setTheme('light');
 
+  useEffect(() => {
+    setIsLoading(true);
+    getData(API_PAINTINGS_ENDPONT).then((data) => setData(data));
+    setIsLoading(false);
+  }, []);
+
   return (
-    <AppContext.Provider value={{ toggleTheme, theme }}>
+    <AppContext.Provider value={{ toggleTheme, theme, isLoading, data }}>
       {children}
     </AppContext.Provider>
   );
