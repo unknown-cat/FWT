9de45764a706af0e34c1268070f2988a8e2ce714
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 19:58:54 +0300

    add styles

---
+++ src/containers/Gallery/gallery.styles.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 90vw;
+  max-width: 1170px;
+  display: grid;
+  grid-gap: 2rem;
+  gap: 1.25rem;
+  margin: 0 auto;
+  padding-bottom: 2.5rem;
+
+  @media screen and (min-width: 992px) {
+    width: 95vw;
+  }
+
+  @media screen and (min-width: 767px) {
+    grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
+  }
+
+  @media screen and (min-width: 1023px) {
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  }
+
+  @media screen and (min-width: 1365px) {
+    grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
+  }
+`;
