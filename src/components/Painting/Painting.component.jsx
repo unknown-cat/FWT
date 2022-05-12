7dc94e7cf4406f53a6a75ffeecc8a1d6e5ee33cf
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 20:02:08 +0300

    add layout and display data

--- src/components/Painting/Painting.component.jsx
+++ src/components/Painting/Painting.component.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Article, Img, Section, H3, Ul, Li } from './painting.styles';
 
-const Painting = () => {
+const Painting = ({ authorId, created, imageUrl, name }) => {
   return (
-    <div>Painting</div>
-  )
-}
+    <Article>
+      <Img
+        src={`https://test-front.framework.team${imageUrl}`}
+        loading='lazy'
+        decoding='async'
+        alt={name}
+        height='360'
+        width='275'
+      />
+      <Section>
+        <H3>{name}</H3>
+        <Ul>
+          <Li>
+            <b>Actor: </b>
+            data
+          </Li>
+          <Li>
+            <b>Created:</b> {created}
+          </Li>
+          <Li>
+            <b>Location: </b>
+            data
+          </Li>
+        </Ul>
+      </Section>
+    </Article>
+  );
+};
 
-export default Painting
+export default Painting;
