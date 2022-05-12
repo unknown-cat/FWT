77c463636f02cf73ba35ad2353359a1d0ed93e7d
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 19:40:59 +0300

    create custom dropdown menu

--- src/components/SelectCreatedDate/SelectCreatedDate.component.jsx
+++ src/components/SelectCreatedDate/SelectCreatedDate.component.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  DropDownContainer,
+  DropDownHeading,
+  DropDownIconUp,
+  DropDownIconDown,
+  DropDownInputContainer,
+  Input,
+} from './selectCreatedDate.styles';
+import { Wrapper } from '../../globalStyles';
 
 const SelectCreatedDate = () => {
-  return (
-    <div>SelectCreatedDate</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default SelectCreatedDate
+  return (
+    <Wrapper>
+      <DropDownContainer>
+        <DropDownHeading onClick={() => setIsOpen(!isOpen)}>
+          <p className='title'>Created</p>
+          {!isOpen ? <DropDownIconUp /> : <DropDownIconDown />}
+        </DropDownHeading>
+        {isOpen ? (
+          <DropDownInputContainer>
+            <label htmlFor='from'></label>
+            <Input
+              type='number'
+              placeholder='from'
+              autoComplete='off'
+              name='from'
+            />
+            <div className='line'></div>
+            <label htmlFor='before'></label>
+            <Input
+              type='number'
+              name='before'
+              placeholder='before'
+              autoComplete='off'
+            />
+          </DropDownInputContainer>
+        ) : null}
+      </DropDownContainer>
+    </Wrapper>
+  );
+};
+
+export default SelectCreatedDate;
