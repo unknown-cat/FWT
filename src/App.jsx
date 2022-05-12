7e79fda797922b3ee9e440b3336b83c9871312bf
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 19:08:27 +0300

    pass theme to themeProvider
    ThemeProvider provides a theme to all React components underneath itself via the context API

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Header, SearchForm, Gallery, Pagintaion } from './containers';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles, { lightTheme, darkTheme } from './globalStyles';
+import { useGlobalContext } from './context.jsx';
 
 export const App = () => {
+  const { theme } = useGlobalContext();
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <GlobalStyles />
       <Header />
       <SearchForm />
