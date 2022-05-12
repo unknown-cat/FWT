b7d442a3aa60bbfea3b88d77b6b1aa864399a78e
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 19:40:28 +0300

    add styles

---
+++ src/components/SelectCreatedDate/selectCreatedDate.styles.jsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import { DropDownIcon } from '../../globalStyles';
+
+export const DropDownHeading = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+
+  @media screen and (max-width: 767px) {
+    flex-direction: row;
+  }
+`;
+
+export const DropDownInputContainer = styled.div`
+  position: absolute;
+  top: 42px;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 2rem;
+  background: ${({ theme }) => theme.searchBg};
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.searchBorder};
+  border-top: none;
+
+  .line {
+    width: 12px;
+    height: 1px;
+    margin: 10px 15px;
+    background: ${({ theme }) => theme.txt};
+  }
+
+  @media screen and (max-width: 767px) {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export const DropDownContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100%;
+  background: ${({ theme }) => theme.searchBg};
+  border: 1px solid ${({ theme }) => theme.searchBorder};
+  border-radius: 8px;
+  overflow: hidden;
+
+  &:hover,
+  &:hover ${DropDownInputContainer} {
+    border-color: ${({ theme }) => theme.border};
+  }
+
+  //Hide Spinners
+  input[type='number']::-webkit-inner-spin-button,
+  input[type='number']::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 15px;
+  max-width: 110px;
+  color: #000000;
+  border: none;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.inputBg};
+
+  &-internal-autofill-selected {
+    background: #000000;
+  }
+
+  &:hover {
+    border-color: ${({ theme }) => theme.txt};
+  }
+
+  @media screen and (max-width: 767px) {
+    max-width: unset;
+    width: 45%;
+  }
+`;
+
+export const DropDownIconUp = styled(DropDownIcon)`
+  fill: ${({ theme }) => theme.searchBorder};
+`;
+export const DropDownIconDown = styled(DropDownIcon)`
+  transform: rotate(180deg);
+`;
