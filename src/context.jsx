343209a954d6750a1509b9bf128b3b2c3f138966
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 18:45:27 +0300

    rename contex -> context

---
+++ src/context.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () =>
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+
+  return (
+    <AppContext.Provider value={{ toggleTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
