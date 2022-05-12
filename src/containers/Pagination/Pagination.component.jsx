b015e1c54de83a1b064be9230856b9d87eaa2e36
unknown-cat <twilightcat73@yahoo.com>
2022-05-12 20:07:06 +0300

    add layout

--- src/containers/Pagination/Pagination.component.jsx
+++ src/containers/Pagination/Pagination.component.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { useGlobalContext } from '../../context';
+import {
+  ButtonWrapper,
+  PaginationWrapper,
+  FirstPageIcon,
+  PrevPageIcon,
+  NextPageIcon,
+  LastPageIcon,
+} from './pagination.styles';
 
 const Pagination = () => {
-  return (
-    <div>Pagination</div>
-  )
-}
+  const { isLoading, data } = useGlobalContext();
 
-export default Pagination
+  if (!data.length) {
+    return (
+      <PaginationWrapper style={{ display: 'flex', justifyContent: 'center' }}>
+        <h2>There is no matches!</h2>
+      </PaginationWrapper>
+    );
+  }
+
+  return (
+    <PaginationWrapper>
+      {!isLoading && (
+        <ButtonWrapper>
+          <button type='button'>
+            <FirstPageIcon />
+          </button>
+          <button type='button'>
+            <PrevPageIcon />
+          </button>
+          {data.map((_, i) => (
+            <button key={i} type='button'>
+              {i + 1}
+            </button>
+          ))}
+          <button type='button'>
+            <NextPageIcon />
+          </button>
+          <button type='button'>
+            <LastPageIcon />
+          </button>
+        </ButtonWrapper>
+      )}
+    </PaginationWrapper>
+  );
+};
+
+export default Pagination;
