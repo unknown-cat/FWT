63d0ca8ca7738cd4bbfd7780bd6a343a7f30af42
unknown-cat <twilightcat73@yahoo.com>
2022-05-15 16:04:22 +0300

    adjust container size and margin between elements

--- src/containers/SearchForm/searchForm.styles.jsx
+++ src/containers/SearchForm/searchForm.styles.jsx
@@ -8,17 +8,21 @@ export const Section = styled.div`
   max-width: 1170px;
   min-height: 70px;
   margin: 5rem auto 2.5rem;
+
+  @media screen and (min-width: 992px) {
+    width: 95vw;
+  }
 `;
 
 export const FormList = styled.form`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  gap: 1rem;
   width: 100%;
   cursor: pointer;
 
   @media screen and (max-width: 767px) {
     flex-direction: column;
+    gap: 1rem;
   }
-`
+`;
